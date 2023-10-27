90addfb98494549503e0c218f5208b706373fc10
Dolf <adolfo.mttgz@gmail.com>
2023-10-27 02:06:35 -0300

    part of third correction

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -32,11 +32,6 @@ def negative_assert_code_400(kit_name):
     # Confirmación de mensaje de error de ingreso de solicitud a la DB
     assert response.json()['message'] == 'Bad Request'
 
-    # Verificar si NO se registra el kit en la DB;
-    # Solicitud->GET-> "var" se compara con la anterior Solicitud->POST-> "response"
-    var = sender_stand_request.get_kit_table().text # variable "var" obtiene el texto de respuesta a la solicitud de la tabla "Kit" de BD
-    assert str(kit_name) not in var # Valor esperado = True / Positivo
-
 # 1° Función de prueba para un carácter (1)
 # Ej. kit_body = { "name": "a"} -- Código de respuesta — 201
 def test_create_kit_for_01_characters_success_response():
